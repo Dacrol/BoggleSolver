4adfd8c7a5ed013fba4b130ab57f4cf66648743d
Dacrol <jonas.ahrens@gmail.com>
2018-07-19 01:34:50 +0200

    Assign start & end methods accordingly

--- timer.js
+++ timer.js
@@ -1,4 +1,8 @@
 class Timer {
+  constructor() {
+    this.start = (typeof process !== 'undefined' && process.hrtime) ? this.nodeStart : (typeof window !== 'undefined' && window.performance) ? this.browserStart : () => {}
+    this.end = (typeof process !== 'undefined' && process.hrtime) ? this.nodeEnd : (typeof window !== 'undefined' && window.performance) ? this.browserEnd : () => {}
+  }
   nodeStart() {
     this.timer = process.hrtime()
   }
