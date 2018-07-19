7bf65cfe258768c63650a2fd7d7c3b47a7d170cd
Dacrol <jonas.ahrens@gmail.com>
2018-07-19 19:25:52 +0200

    Split matrix & start position in return object

--- solver.js
+++ solver.js
@@ -3,29 +3,34 @@ const fs = require('fs')
 readAndSplitWordlist().catch(error => {
   console.error(error)
 })
-readMatrix('piggy.txt').catch(error => {
-  console.error(error)
-})
+readMatrix('piggy.txt')
+  .catch(error => {
+    console.error(error)
+  })
+  .then(result => console.log(result))
 
 async function readAndSplitWordlist(filename = './wordlist.txt') {
   const wordlist = (await loadFile(filename).catch(() => {
     throw new Error('Failed to load wordlist')
   }))
+    .trim()
     .toLowerCase()
     .split(/\r?\n/)
     .map(word => word.trim())
-  console.log(wordlist)
   return wordlist
 }
 
 async function readMatrix(filename) {
   const rawText = (await loadFile(filename).catch(() => {
     throw new Error('Failed to load matrix')
-  })).trim()
+  }))
+    .trim()
+    .toLowerCase()
   const linesArray = rawText.split(/\r?\n/).map(line => line.trim().split(' '))
-  const mazetrix = linesArray.slice(1)
-  // console.log(linesArray)
-  console.log(mazetrix)
+  return {
+    matrix: linesArray.slice(1),
+    startPosition: [+linesArray[0][1] - 1, +linesArray[0][2] - 1]
+  }
 }
 
 function loadFile(filename) {
