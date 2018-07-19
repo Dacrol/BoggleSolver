7787baa4bb534b883b0819de172b0015269ff34d
Dacrol <jonas.ahrens@gmail.com>
2018-07-19 17:29:58 +0200

    Lowercase and trim all words in wordlist

--- solver.js
+++ solver.js
@@ -1,19 +1,28 @@
 const fs = require('fs')
 
-readAndSplitWordlist().catch((error) => {console.error(error)})
-readMatrix('piggy.txt').catch((error) => {console.error(error)})
+readAndSplitWordlist().catch(error => {
+  console.error(error)
+})
+readMatrix('piggy.txt').catch(error => {
+  console.error(error)
+})
 
 async function readAndSplitWordlist(filename = './wordlist.txt') {
-  const wordlist = (await loadFile(filename).catch(() => {throw new Error('Failed to load wordlist')})).split(
-    /\r?\n/
-  )
+  const wordlist = (await loadFile(filename).catch(() => {
+    throw new Error('Failed to load wordlist')
+  }))
+    .toLowerCase()
+    .split(/\r?\n/)
+    .map(word => word.trim())
   console.log(wordlist)
   return wordlist
 }
 
 async function readMatrix(filename) {
-  const rawText = (await loadFile(filename).catch(()=>{throw new Error('Failed to load matrix')})).trim()
-  const linesArray = rawText.split(/\r?\n/).map((line) => line.trim().split(' '))
+  const rawText = (await loadFile(filename).catch(() => {
+    throw new Error('Failed to load matrix')
+  })).trim()
+  const linesArray = rawText.split(/\r?\n/).map(line => line.trim().split(' '))
   const mazetrix = linesArray.slice(1)
   // console.log(linesArray)
   console.log(mazetrix)
